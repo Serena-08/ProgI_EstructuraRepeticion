d41cd3797aa2d84d09d962f69ee35478d2b45aea
ISP63 <serefisch17@gmail.com>
2026-04-09 20:54:10 -0300

    Commit

---
+++ src/edu/ies63/progI/cicloPara.java
@@ -0,0 +1,15 @@
+package edu.ies63.progI;
+
+public class cicloPara {
+  public static void main(String[] args) {
+
+    /* for (para) son siclos determinados porque sabemos la cantidad de iteraciones
+    Sintaxis:
+    for(inicializador; condicion; incremento){
+    }
+     */
+
+    for(int i = 0; )
+
+  }
+}
